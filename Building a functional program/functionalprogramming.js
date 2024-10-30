409c6f46c35e6e0b80de3c950551f8edb5a7a71a
huipan18 <114424568+huipan18@users.noreply.github.com>
2024-10-31 00:38:22 +0530

    Update functionalprogramming.js

--- Building a functional program/functionalprogramming.js
+++ Building a functional program/functionalprogramming.js
@@ -2,7 +2,7 @@
 function consoleStyler(color,background ,fontSize,txt) {
     var message = "%c" + txt;
     var style = `color: ${color};`
-    style += `background: ${background};`
+    style += `background: ${background};'
     style += `font-size: ${fontSize};`
     console.log(message, style);
 }
